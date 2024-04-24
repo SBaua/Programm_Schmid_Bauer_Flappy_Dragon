f1c9c439eadc49c9e3a94a81af229a2b410380fa
Simon Bauer <simon.bauer@josephinum.at>
2024-04-24 15:25:15 +0200

    sync johann

--- Programm_Schmid_Bauer_Flappy_Dragon/MainWindow.xaml.cs
+++ Programm_Schmid_Bauer_Flappy_Dragon/MainWindow.xaml.cs
@@ -20,7 +20,6 @@ namespace Programm_Schmid_Bauer_Flappy_Dragon
 
         DispatcherTimer gameTimer = new DispatcherTimer();
 
-
         double score;
         int gravity = 8;
         bool gameOver;
@@ -35,7 +34,6 @@ namespace Programm_Schmid_Bauer_Flappy_Dragon
             StartGame();
 
         }
-
         private void MainEventTimer(object sender, EventArgs e)
         {
             txtScore.Content = "Score: " + score;
@@ -48,8 +46,6 @@ namespace Programm_Schmid_Bauer_Flappy_Dragon
             {
                 EndGame();
             }
-
-
             foreach (var x in MyCanvas.Children.OfType<Image>())
             {
                 if ((string)x.Tag == "obs1" || (string)x.Tag == "obs2" || (string)x.Tag == "obs3")
@@ -62,7 +58,6 @@ namespace Programm_Schmid_Bauer_Flappy_Dragon
 
                         score += .5;
                     }
-
                     Rect PillarHitBox = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
 
                     if (flappyBirdHitBox.IntersectsWith(PillarHitBox))
@@ -70,7 +65,6 @@ namespace Programm_Schmid_Bauer_Flappy_Dragon
                         EndGame();
                     }
                 }
-
                 if ((string)x.Tag == "clouds")
                 {
                     Canvas.SetLeft(x, Canvas.GetLeft(x) - 1);
@@ -81,15 +75,9 @@ namespace Programm_Schmid_Bauer_Flappy_Dragon
 
                         score += .5;
                     }
-
                 }
-
-
             }
-
-
         }
-
         private void KeyIsDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Space)
@@ -103,14 +91,12 @@ namespace Programm_Schmid_Bauer_Flappy_Dragon
                 StartGame();
             }
         }
-
         private void KeyIsUp(object sender, KeyEventArgs e)
         {
             flappyBird.RenderTransform = new RotateTransform(5, flappyBird.Width / 2, flappyBird.Height / 2);
 
             gravity = 8;
         }
-
         private void StartGame()
         {
             MyCanvas.Focus();
@@ -137,17 +123,14 @@ namespace Programm_Schmid_Bauer_Flappy_Dragon
                 {
                     Canvas.SetLeft(x, 1100);
                 }
-
                 if ((string)x.Tag == "clouds")
                 {
                     Canvas.SetLeft(x, 300 + temp);
                     temp = 800;
                 }
             }
-
             gameTimer.Start();
         }
-
         private void EndGame()
         {
             gameTimer.Stop();
@@ -157,4 +140,3 @@ namespace Programm_Schmid_Bauer_Flappy_Dragon
         }
     }
 }
-
