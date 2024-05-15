8a69c4ef44b850c52e9bf51de76326e943d2e741
Simon Bauer <simon.bauer@josephinum.at>
2024-05-15 17:40:17 +0200

    fix with bird hitbox

--- Programm_Schmid_Bauer_Flappy_Dragon/MainWindow.xaml.cs
+++ Programm_Schmid_Bauer_Flappy_Dragon/MainWindow.xaml.cs
@@ -99,7 +99,7 @@ namespace Programm_Schmid_Bauer_Flappy_Dragon
         }
         private void KeyIsUp(object sender, KeyEventArgs e)
         {
-            flappyBird.RenderTransform = new RotateTransform(5, flappyBird.Width / 2, flappyBird.Height / 2);
+            flappyBird.RenderTransform = new RotateTransform(5, bird_hitbox.Width / 2, bird_hitbox.Height / 2);
 
             gravity = 5;
         }
